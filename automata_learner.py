e2107c1eb7b8fb11393d296175e9dfd747fe67f0
livialestingi <livia.lestingi@polimi.it>
2024-02-15 10:15:28 +0100

    fix learned automaton naming

--- automata_learner.py
+++ automata_learner.py
@@ -1,3 +1,3 @@
 from it.polimi.sha_learning.autotwin_learn import learn_automaton
 
-learn_automaton('item', '2023-11-04-13-0-0', '2023-11-04-14-3-0')
+learn_automaton('item', '2023-11-04-13-0-0', '2023-11-04-14-2-0')
