71f7767b3f065b834726e6974516171d09cc4d99
livialestingi <livia.lestingi@polimi.it>
2024-02-15 16:54:58 +0100

    adding primitives for automaton creation and deletion

--- automata_learner.py
+++ automata_learner.py
@@ -1,9 +1,9 @@
-from it.polimi.sha_learning.autotwin_learn import learn_automaton
 from autotwin_connector import store_automaton, delete_automaton
+from it.polimi.sha_learning.autotwin_learn import learn_automaton
 
 # Testing automaton learning.
 learned_sha = learn_automaton('item', '2023-11-04-13-0-0', '2023-11-04-14-2-0')
 # Deletes learned automaton from the SKG, if it already exists.
-# delete_automaton(learned_sha)
+delete_automaton(learned_sha)
 # Stores the learned automaton into the SKG.
-# store_automaton(learned_sha)
+store_automaton(learned_sha)
