a9447e1baf784227e764a572baafa05d444a76b5
livialestingi <livia.lestingi@polimi.it>
2024-02-26 14:35:56 +0100

    wip fixes for V4

--- automata_learner.py
+++ automata_learner.py
@@ -3,13 +3,13 @@ from autotwin_mapper import write_semantic_links
 from it.polimi.sha_learning.autotwin_learn import learn_automaton
 
 # 1: Automata Learning experiment.
-learned_sha = learn_automaton('item', start_ts=0, end_ts=100000000)
+learned_sha = learn_automaton('item', start_ts=0, end_ts=1000000)
 
 # 2: Delete learned automaton from the SKG, if there already exists one with the same name.
-# delete_automaton(learned_sha)
+delete_automaton(learned_sha)
 
 # 3: Store the learned automaton into the SKG.
-# store_automaton(learned_sha)
+store_automaton(learned_sha)
 
 # 4: Create semantic links between learned model and existing SKG nodes.
-# write_semantic_links(learned_sha)
+write_semantic_links(learned_sha)
