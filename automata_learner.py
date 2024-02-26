9fb7b8414119796592983245b780cbabcaa275cd
livialestingi <livia.lestingi@polimi.it>
2024-02-26 17:08:16 +0100

    fixes for package release

--- automata_learner.py
+++ automata_learner.py
@@ -1,9 +1,9 @@
-from autotwin_connector import store_automaton, delete_automaton
 from autotwin_mapper import write_semantic_links
 from it.polimi.sha_learning.autotwin_learn import learn_automaton
+from skg_main.autotwin_connector import store_automaton, delete_automaton
 
 # 1: Automata Learning experiment.
-learned_sha = learn_automaton('item', start_ts=0, end_ts=1000000)
+learned_sha = learn_automaton('item', start_ts=0, end_ts=100000)
 
 # 2: Delete learned automaton from the SKG, if there already exists one with the same name.
 delete_automaton(learned_sha)
